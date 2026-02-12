09981cf0433653de18c4e320edf4e8dc62cbc313
Athorobban <azmiatho@gmail.com>
2026-02-12 20:26:21 +0700

    initial commit 2

--- main.go
+++ main.go
@@ -18,12 +18,17 @@ func main() {
 
 	seed.SeedAdmin()
 	app := fiber.New()
-
+	// user setup
 	userRepo := repositories.NewUserRepository()
 	userService := services.NewUserService(userRepo)
 	userController := controllers.NewuserController(userService)
 
-	routes.Setup(app, userController)
+	// board setup
+	boardRepo := repositories.NewBoardRepository()
+	boardService := services.NewBoardService(boardRepo,userRepo)
+	boardController := controllers.NewBoardController(boardService)
+
+	routes.Setup(app, userController, boardController)
 
 	port := config.AppConfig.AppPort
 	log.Println("Server is running on port :", port)
